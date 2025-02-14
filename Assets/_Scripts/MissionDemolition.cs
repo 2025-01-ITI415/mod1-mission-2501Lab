3fd45e6adcd1fb9d57d35dd6dd874e5ee7f92ca5
ksw-92 <ksw92@scarletmail.rutgers.edu>
2025-02-13 20:32:45 -0500

    added mission demolition script

---
+++ Assets/_Scripts/MissionDemolition.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameMode
+{
+    idle,
+    playing,
+    levelEnd
+}
+
+public class MissionDemolition : MonoBehaviour
+{
+    static private MissionDemolition S; // a private Singleton
+
+    [Header("Inscribed")]
+    public Text uitLevel;
+    public Text uitShots;
+    public Vector3 castlePos;
+    public GameObject[] castles;
+
+    [Header("Dynamic")]
+    public int level;
+    public int levelMax;
+    public int shotsTaken;
+    public GameObject castle;
+    public GameMode mode = GameMode.idle;
+    public string showing = "Show Slingshot";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
