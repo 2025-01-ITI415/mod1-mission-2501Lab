a50f8d262374d85de8d45b92a0688fc168903b45
Jonah Sosis <jonahsosis@gmail.com>
2025-02-16 23:13:13 -0500

    finished

--- Assets/__Scripts/FollowCam.cs
+++ Assets/__Scripts/FollowCam.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FollowCam : MonoBehaviour
@@ -16,8 +17,27 @@ public class FollowCam : MonoBehaviour
     }
     void FixedUpdate()
     {
+        /*
         if (POI == null) return;
         Vector3 destination = POI.transform.position;
+        */
+        Vector3 destination;
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            destination = POI.transform.position;
+            if(POI.tag=="Projectile")
+            {
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    POI = null;
+                    return;
+                }
+            }
+        }
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
