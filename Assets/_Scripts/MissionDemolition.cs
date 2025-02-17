3e97e0be0360d2674980fcb1d59b40438a387ff6
ksw-92 <ksw92@scarletmail.rutgers.edu>
2025-02-16 19:38:21 -0500

    added switch view button

--- Assets/_Scripts/MissionDemolition.cs
+++ Assets/_Scripts/MissionDemolition.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public enum GameMode
 {
@@ -15,8 +16,8 @@ public class MissionDemolition : MonoBehaviour
     static private MissionDemolition S; // a private Singleton
 
     [Header("Inscribed")]
-    public Text uitLevel;
-    public Text uitShots;
+    public TextMeshProUGUI uitLevel;
+    public TextMeshProUGUI uitShots;
     public Vector3 castlePos;
     public GameObject[] castles;
 
@@ -31,12 +32,67 @@ public class MissionDemolition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        S = this;
+
+        level = 0;
+        shotsTaken = 0;
+        levelMax = castles.Length;
+        StartLevel();
+    }
+
+    void StartLevel()
+    {
+        if (castle != null)
+        {
+            Destroy(castle);
+        }
+        Projectile.DESTROY_PROJECTILES();
+
+        castle = Instantiate<GameObject>(castles[level]);
+        castle.transform.position = castlePos;
+
+        Goal.goalMet = false;
+
+        UpdateGUI();
+
+        mode = GameMode.playing;
     }
 
     // Update is called once per frame
-    void Update()
+    void UpdateGUI()
     {
-        
+        uitLevel.text = "Level: " + (level + 1) + " of" + levelMax;
+        uitShots.text = "Shots Taken: " + shotsTaken;
+    }
+    private void Update()
+    {
+        UpdateGUI();
+
+        if ( (mode == GameMode.playing) && Goal.goalMet ) {
+
+            mode = GameMode.levelEnd;
+
+            Invoke("NextLevel", 2f);
+        }
+    }
+    void NextLevel()
+    {
+        level++;
+        if (level == levelMax)
+        {
+            level = 0;
+            shotsTaken = 0;
+        }
+        StartLevel();
+    }
+
+    static public void SHOT_FIRED()
+    {
+        S.shotsTaken++;
+    }
+
+    static public GameObject GET_CASTLE()
+    {
+        return S.castle;
     }
 }
