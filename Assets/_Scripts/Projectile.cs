f3fea08263fb723b407fbeab6cfab4f4555aede0
Daniel H <danielmople1@gmail.com>
2025-02-16 23:06:29 -0500

    Added MissionDemolition script and adding method to Projectile.cs

--- Assets/_Scripts/Projectile.cs
+++ Assets/_Scripts/Projectile.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour{
     const int LOOKBACK_COUNT = 10;
+    static List<Projectile> PROJECTILES = new List<Projectile>();
 
     [SerializeField]
     private bool _awake = true;
@@ -23,6 +24,7 @@ public class Projectile : MonoBehaviour{
         awake = true;
         prevPos = new Vector3(1000,1000,0);
         deltas.Add( 1000 );
+        PROJECTILES.Add( this );
     }
 
     // Update is called once per frame
@@ -48,5 +50,9 @@ public class Projectile : MonoBehaviour{
         }
 
 
+    }
+
+    private void OnDestroy(){
+        PROJECTILES.Remove( this );
     }
 }
