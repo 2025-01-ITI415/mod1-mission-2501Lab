45b586fb26379bfb62f15e1488c0ad6821456eab
Daniel H <danielmople1@gmail.com>
2025-02-16 23:13:59 -0500

    Can now load the next level and delete the old castle

--- Assets/_Scripts/Projectile.cs
+++ Assets/_Scripts/Projectile.cs
@@ -55,4 +55,10 @@ public class Projectile : MonoBehaviour{
     private void OnDestroy(){
         PROJECTILES.Remove( this );
     }
+    
+    static public void DESTROY_PROJECTILES(){
+        foreach ( Projectile p in PROJECTILES){
+            Destroy( p.gameObject );
+        }
+    }
 }
